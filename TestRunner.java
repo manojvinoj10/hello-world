5dba95d766fb0cde637d8cd6ee34dfeeefadcd71
Manoj Kannan <65971937+manojvinoj10@users.noreply.github.com>
2025-09-04 01:58:55 +0530

    Create TestRunner.java

---
+++ TestRunner.java
@@ -0,0 +1,7 @@
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="features", glue={"src/test/java"})
+public class TestRunner {}
